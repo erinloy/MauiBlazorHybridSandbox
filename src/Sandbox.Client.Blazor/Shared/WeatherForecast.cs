c4324c89af2cc446641dcdb79fe404b450221a07
Erin Loy <eloy@crazydogventures.com>
2022-07-18 15:55:33 -0700

    Add project files.

---
+++ src/Sandbox.Client.Blazor/Shared/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Sandbox.Client.Blazor.Shared
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public string? Summary { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
---
+++ src/Sandbox.Client.Maui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Sandbox.Client.Maui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
